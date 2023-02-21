cfb2433664558b536ab7858b28cbdb855a01f9a6
shun_a123 <sshuna123@gmail.com>
2023-02-22 06:14:03 +0900

    [add] プレイヤー挙動追加、Asset(マップ関連)追加

--- Assets/Script/Ride.cs
+++ Assets/Script/Ride.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Ride : MonoBehaviour
 {
     [SerializeField] float _speed = 5;
-    [SerializeField] float _rotateSpeed = 20;
+    [SerializeField] float _rotateSpeed = 200;
+    [SerializeField] float _limitSpeed = 150;
+    [SerializeField] float _currentSpeed;
     [SerializeField] Camera _cam;
     [SerializeField] GameObject _playerObj;
     Rigidbody _rb;
@@ -24,12 +26,13 @@ public class Ride : MonoBehaviour
     {
         Ray ray = new Ray(transform.position, Vector3.down);
         RaycastHit rayHit;
-        int range = 10;
+        float range = 0.9f;
         Debug.DrawRay(ray.origin, ray.direction, Color.red);
         if(Physics.Raycast(ray,out rayHit,range))
         {
             Debug.Log(rayHit.collider.gameObject.name + rayHit.normal);
 
+            _rb.useGravity = false;
             // 法線ベクトルの取得、角度をベクトルと垂直に
             // localPosition.yを少し上に、浮遊させる
             Vector3 rotation = this.transform.eulerAngles;
@@ -41,6 +44,21 @@ public class Ride : MonoBehaviour
                 RideMove(plusFloat, rotation);
             }
         }
+        else
+        {
+            _rb.useGravity = true;
+            if (_isRide == true)
+            {
+                AirMove();
+            }
+        }
+
+        if (_rb.velocity.magnitude > _limitSpeed)
+        {
+            _rb.velocity = new Vector3(_rb.velocity.x / 1.05f, _rb.velocity.y, _rb.velocity.z / 1.05f);
+        }
+        _currentSpeed = _rb.velocity.magnitude;
+        Debug.Log(_currentSpeed);
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -65,5 +83,13 @@ public class Ride : MonoBehaviour
         _rb.AddRelativeForce(Vector3.forward * verticalSpd);
         transform.localPosition = yPosition;
         transform.eulerAngles += new Vector3(0, horizontalSpd * Time.deltaTime , 0) + nVector;
+    }
+
+    void AirMove()
+    {
+        float verticalSpd = _speed * Input.GetAxisRaw("Vertical");
+        float horizontalSpd = _rotateSpeed * Input.GetAxisRaw("Horizontal");
+        _rb.AddRelativeForce(Vector3.forward * verticalSpd);
+        transform.eulerAngles += new Vector3(0, horizontalSpd * Time.deltaTime, 0);
     }
 }
